5ab99b566450bf85f773e334ba3504d9492d8667
maheswarRamisetty <maheshwarramisetty184@gmail.com>
2026-02-09 00:01:36 +0530

    Project Initialization

---
+++ shared/common-events/src/main/java/com/mahesh/arch/shared/App.java
@@ -0,0 +1,13 @@
+package com.mahesh.arch.shared;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
