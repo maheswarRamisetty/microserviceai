f8d1ce6f2715ce19d2c2001d6f0d5f59b1d92b42
ramisettymahesh <maheshwarramisetty184@gmail.com>
2026-02-19 12:04:25 +0530

    Add config for kafkaTemplate

--- services/repo-scanner-service/src/main/java/com/mahesh/arch/RepoScannerApplication.java
+++ services/repo-scanner-service/src/main/java/com/mahesh/arch/RepoScannerApplication.java
@@ -3,9 +3,12 @@ package com.mahesh.arch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 import java.util.List;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.mahesh.arch.config")
 public class RepoScannerApplication {
     public static void main(String[] args){
         SpringApplication.run(RepoScannerApplication.class,args);
